e7244a67a5b4d6c8a445a427860ac8bcb367a303
SiChuchen <m17609384279@163.com>
2026-04-11 23:06:52 +0800

    build: scaffold dotnet control core solution

---
+++ tests/TermForge.Core.Tests/StatusServiceTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace TermForge.Core.Tests;
+
+public class StatusServiceTests
+{
+    [Fact]
+    public void Solution_bootstrap_test_project_compiles()
+    {
+        Assert.True(true);
+    }
+}
